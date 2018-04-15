744f3b7c0bd6ca28156f9152ca45874cc4b32f53
divkum@outlook.com <divkum@outlook.com>
2018-04-14 18:16:29 -0700

    initial program

--- OnlineTestSystemApp/Program.cs
+++ OnlineTestSystemApp/Program.cs
@@ -10,6 +10,35 @@ namespace OnlineTestSystemApp
     {
         static void Main(string[] args)
         {
+            // Create Question
+            var question = new Question("what is the value of pi?", "2.14", "3.14", "4.14", "5.14", 2);
+
+            Console.WriteLine($"Q{ question.QuestionNumber }: {question.QuestionText}");
+            Console.WriteLine($"1) {question.option1}  2) { question.option2}   3) { question.option3}  4) { question.option4}");
+
+
+
+            //Console.WriteLine($"2) {question.option2}");
+            //Console.WriteLine($"3) {question.option3}");
+            //Console.WriteLine($"4) {question.option4}");
+            //Console.Write("Select Answer: ");
+            string selectedOption = Console.ReadLine();
+
+            // Answer the question
+            question.AnswerTheQuestion(int.Parse(selectedOption));
+
+            // Evaluate
+            question.Evaluate();
+
+            // Print whether correct or wrong
+            if (question.isAnsweredCorrect == true)
+            {
+                Console.WriteLine("Correct!!");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect. The correct option is {question.correctOption}");
+            }
         }
     }
 }
--- OnlineTestSystemApp/Question.cs
+++ OnlineTestSystemApp/Question.cs
@@ -9,44 +9,86 @@ namespace OnlineTestSystemApp
    
         class Question
         {
+        //    private static int lastQuestionNumber = 1;
+        //    public int QuestionNumber { get; set; }
+        //    public string option1 { get; set; }
+        //    public string option2 { get; set; }
+        //    public string option3 { get; set; }
+        //    public string option4 { get; set; }
+        //    public int correctOption { get; set; }
+        //    public int markedOption { get; set; }
+
+        //    #region Constructor
+        //    public Question()
+        //    {
+        //        QuestionNumber = lastQuestionNumber++;
+
+        //    }
+        //    #endregion
+
+        //    #region Methods
+
+        //    public int MarksObtained(int QuestionNumber, int option1, int option2, int option3, int option4, int correctOption, int markedOption)
+        //    {
+        //        int totalMarks = 0;
+        //        if (markedOption == correctOption)
+        //        {
+        //            totalMarks = totalMarks + 3;
+        //        }
+        //        else
+        //        {
+        //            totalMarks = totalMarks - 1;
+
+        //        }
+        //        return totalMarks;
+
+
+        //    }
+        //#endregion
+        
             private static int lastQuestionNumber = 1;
-            public int QuestionNumber { get; set; }
-            public string option1 { get; set; }
-            public string option2 { get; set; }
-            public string option3 { get; set; }
-            public string option4 { get; set; }
+            public int QuestionNumber { get; private set; }
+            public string QuestionText { get; set; }
+            public string option1 { get; private set; }
+            public string option2 { get; private set; }
+            public string option3 { get; private set; }
+            public string option4 { get; private set; }
             public int correctOption { get; set; }
             public int markedOption { get; set; }
+            public bool? isAnsweredCorrect { get; set; }
 
-            #region Constructor
-            public Question()
+            public Question(string QuestionText, string option1, string option2, string option3, string option4, int correctOption)
             {
-                QuestionNumber = lastQuestionNumber++;
+                this.QuestionNumber = lastQuestionNumber++;
+                this.QuestionText = QuestionText;
+                this.option1 = option1;
+                this.option2 = option2;
+                this.option3 = option3;
+                this.option4 = option4;
 
+                this.correctOption = correctOption;
             }
-            #endregion
 
-            #region Methods
-
-            public int MarksObtained(int QuestionNumber, int option1, int option2, int option3, int option4, int correctOption, int markedOption)
+            public void AnswerTheQuestion(int markedOption)
             {
-                int totalMarks = 0;
-                if (markedOption == correctOption)
+                this.markedOption = markedOption;
+            }
+
+            public void Evaluate()
+            {
+                if (this.markedOption == this.correctOption)
                 {
-                    totalMarks = totalMarks + 3;
+                    isAnsweredCorrect = true;
                 }
                 else
                 {
-                    totalMarks = totalMarks - 1;
-
+                    isAnsweredCorrect = false;
                 }
-                return totalMarks;
-
-
             }
-            #endregion
-
         }
 
-    
 }
+
+
+    
+
